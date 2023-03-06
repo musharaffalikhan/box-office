38743a5b80d32afeba0cfba24431c7ecdaa49375
musharaffalikhan <96779266+musharaffalikhan@users.noreply.github.com>
2023-03-06 18:02:20 +0530

    Using useQuery hook in home component

--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -1,28 +1,45 @@
 import React, { useState } from "react";
 import { searchForActors, searchForShows } from "../Api/ApiService";
+import { useQuery } from "@tanstack/react-query";
 
 import Search from "../Components/Search";
 import ActorsGrid from "../Components/Shows and actors/ActorsGrid";
 import ShowGrid from "../Components/Shows and actors/ShowGrid";
 
-
 const Homepage = () => {
-  const [apiData, setApiData] = useState(null);
-  const [apiError, setApiError] = useState(null);
 
-  const onSearchSubmitHandler = async ({ q, searchOption }) => {
-    try {
-      setApiError(null);
-      if (searchOption === "shows") {
-        const res = await searchForShows(q);
-        setApiData(res);
-      } else {
-        const res = await searchForActors(q);
-        setApiData(res);
-      }
-    } catch (error) {
-      setApiError(error);
-    }
+  // Normal Data fetching method
+  // const [apiData, setApiData] = useState(null);
+  // const [apiError, setApiError] = useState(null);
+  // const onSearchSubmitHandler = async ({ q, searchOption }) => {
+  //   try {
+  //     setApiError(null);
+  //     if (searchOption === "shows") {
+  //       const res = await searchForShows(q);
+  //       setApiData(res);
+  //     } else {
+  //       const res = await searchForActors(q);
+  //       setApiData(res);
+  //     }
+  //   } catch (error) {
+  //     setApiError(error);
+  //   }
+  // };
+
+
+  // using react query
+  const [filter, setFilter] = useState(null);
+
+  const { data:apiData, error:apiError } = useQuery({
+    queryKey: ["search", filter],
+    queryFn: () =>
+      filter.searchOption === "shows"
+        ? searchForShows(filter.q)
+        : searchForActors(filter.q),
+    enabled: !!filter,
+  });
+  const onSearchSubmitHandler = ({ q, searchOption }) => {
+    setFilter({ q, searchOption });
   };
 
   const renderApiData = () => {
