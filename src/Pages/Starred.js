0116af6b202bcf1dbd1f79f0feb1fc8cae505d2c
musharaffalikhan <96779266+musharaffalikhan@users.noreply.github.com>
2023-03-09 14:10:39 +0530

    Fixes

--- src/Pages/Starred.js
+++ src/Pages/Starred.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useSelector ,useDispatch} from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { useQuery } from "@tanstack/react-query";
 import { getShowsByIds } from "../Api/ApiService";
 import ShowGrid from "../Components/Shows and actors/ShowGrid";
@@ -7,7 +7,7 @@ import { useEffect } from "react";
 import { starredSliceActions } from "../Store/StarredSlice";
 
 const Starred = () => {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
   const starredShowsIds = useSelector((state) => state.starred.ids);
   const { data: starredShows, error: starredShowsError } = useQuery({
     queryKey: ["starred", starredShowsIds],
@@ -17,16 +17,16 @@ const Starred = () => {
       ),
     refetchOnWindowFocus: false,
   });
-  useEffect(()=>{
-    const storedIds = localStorage.getItem('starredShow');
-    if(storedIds){
-      dispatch(starredSliceActions.setIds(JSON.parse(storedIds)))
+  useEffect(() => {
+    const storedIds = localStorage.getItem("starredShow");
+    if (storedIds) {
+      dispatch(starredSliceActions.setIds(JSON.parse(storedIds)));
     }
-  },[dispatch]);
+  }, [dispatch]);
 
-if(starredShows?.length === 0){
-    return <div>No shows are starred</div>
-}
+  if (starredShows?.length === 0) {
+    return <div>No shows are starred</div>;
+  }
 
   if (starredShows?.length > 0) {
     return <ShowGrid shows={starredShows} />;
